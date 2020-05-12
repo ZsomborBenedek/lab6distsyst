504535d120b64dd1993db09a85b34b1c71c76847
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-05-12 17:54:08 +0200

    Create UDPHandler.java

---
+++ namingserver/src/main/java/be/uantwerpen/namingserver/controller/UDPHandler.java
@@ -0,0 +1,92 @@
+package be.uantwerpen.namingserver.controller;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+import be.uantwerpen.namingserver.database.ServerRepository;
+
+public class UDPHandler implements Runnable {
+
+    private ServerRepository repo;
+
+    public UDPHandler(ServerRepository repo) {
+        this.repo = repo;
+
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String name = inetAddress.getHostName();
+            String thisIp = inetAddress.getHostAddress();
+            System.out.println("Name: " + name);
+            System.out.println("IP: " + thisIp);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.out.println("Could not determine host IP address!");
+        }
+        System.out.println("--Started--");
+    }
+
+    public void sendUDPMessage(String message, String ipAddress, int port) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress group = InetAddress.getByName(ipAddress);
+        byte[] msg = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(msg, msg.length, group, port);
+        socket.send(packet);
+        socket.close();
+    }
+
+    public void receiveUDPMessageHandler(int port) throws IOException {
+        byte[] buffer = new byte[1024];
+        MulticastSocket socket = new MulticastSocket(port);
+        InetAddress group = InetAddress.getByName("230.0.0.0");
+        socket.joinGroup(group);
+        while (true) {
+            System.out.println("Waiting for multicast message...");
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            String msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
+            if (msg.contains("next "))
+                System.out.println("next forwarded: " + msg);
+            if (msg.contains("previous "))
+                System.out.println("previous forwarded: " + msg);
+            if (msg.contains("newNode")) {
+                String haha = msg.replace("newNode ", "");
+                if (!haha.isEmpty()) {
+                    String[] tokens = haha.split("::");
+                    repo.addNode(tokens[0], tokens[1]);
+                }
+                System.out.println("Node added: " + haha);
+                System.out.println("NodeCount is " + repo.getNodes().size());
+                sendUDPMessage("nodeCount " + repo.getNodes().size(), "230.0.0.0", 10000);
+            }
+            if (msg.contains("remNode")) {
+                String haha = msg.replace("remNode ", "");
+                if (!haha.isEmpty()) {
+                    String[] tokens = haha.split("::");
+                    repo.removeNode(tokens[0]);
+                }
+                System.out.println("Node removed: " + haha);
+                System.out.println("NodeCount is " + repo.getNodes().size());
+                sendUDPMessage("nodeCount " + repo.getNodes().size(), "230.0.0.0", 10000);
+            }
+            if ("OK".equals(msg)) {
+                System.out.println("No more message. Exiting : " + msg);
+                break;
+            }
+        }
+        socket.leaveGroup(group);
+        socket.close();
+    }
+
+    @Override
+    public void run() {
+        try {
+            receiveUDPMessageHandler(10000);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
